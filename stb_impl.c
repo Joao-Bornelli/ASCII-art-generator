8d8b3e4bdb3089b6e2b437ff235e5463cf71f1ba
Joao Bornelli <joao.bornelli@edu.sc.senai.br>
2025-12-11 08:17:40 -0300

    fix: Fixed selection of char to print if color is 100% black

--- stb_impl.c
+++ stb_impl.c
@@ -1,5 +1,9 @@
+#ifndef STB_IMAGE_IMPLEMENTATION
 #define STB_IMAGE_IMPLEMENTATION
+#endif
 #include "stb/stb_image.h"
 
+#ifndef STB_IMAGE_RESIZE2_IMPLEMENTATION
 #define STB_IMAGE_RESIZE2_IMPLEMENTATION
+#endif
 #include "stb/stb_image_resize2.h"
